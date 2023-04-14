86742792ad0b1a725f36335e357177ecb13933fc
Ben6jamin <b.ishimwe1@alustudent.com>
2023-04-14 15:04:39 +0200

    yesss

--- elementary_programing/Computechange.java
+++ elementary_programing/Computechange.java
@@ -1,12 +1,14 @@
 import java.util.Scanner;
 
-public class counting_monetary {
+public class Computechange {
     double money;
 
     Scanner dr = new Scanner(System.in);
 
     System.out.println("enter amount of money");
 
-    money = dr.nextDouble();
+    money=dr.nextDouble();
+
+    double Newamount =
 
 }
