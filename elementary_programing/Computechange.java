49beacb7f7bc36c6f11ab9be4ebb7f2c5cd78901
Ben6jamin <b.ishimwe1@alustudent.com>
2023-04-14 16:03:17 +0200

    wqqq

--- elementary_programing/Computechange.java
+++ elementary_programing/Computechange.java
@@ -4,25 +4,35 @@ public class ComputeChange {
 
     public static void main(String[] args) {
 
-        double money;
+
 
         Scanner dr = new Scanner(System.in);
 
         System.out.println("enter amount of money");
 
-        money = dr.nextDouble();
+        double money = dr.nextDouble();
 
-        double cents = money * 100;
+        int remainingcents = (int) (money * 100);
 
-        double dollars = cents % 100;
+        int numberdollars = remainingcents / 100;
+        remainingcents = remainingcents % 100;
 
-        double quarters = dollars % 25;
+        int numberquarters = remainingcents / 25;
+        remainingcents = remainingcents % 25;
 
-        double dimes = quarters % 10;
+        int numberdimes = remainingcents / 10;
+        remainingcents = remainingcents % 10;
 
-        double pennies = dimes % 5;
+        int numbernickles = remainingcents / 5;
+        remainingcents = remainingcents % 5;
 
-        System.out.println("here is " + pennies);
+        int numberpennies = remainingcents;
+
+        System.out.println("number of dollars is " + numberdollars);
+        System.out.println("number of quarters is " + numberquarters);
+        System.out.println("number of dimes is " + numberdimes);
+        System.out.println("number of nickles is " + numbernickles);
+        System.out.println("number of pennies is " + numberpennies);
 
     }
 }
