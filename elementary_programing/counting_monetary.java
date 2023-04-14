9a315a1898357df6d73cc2e9f4e9cf59adbe4640
Ben6jamin <b.ishimwe1@alustudent.com>
2023-04-14 14:52:30 +0200

    deleted

---
+++ elementary_programing/counting_monetary.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class counting_monetary {
+    double money;
+
+    Scanner dr = new Scanner(System.in);
+
+    System.out.println("enter amount of money");
+
+    money = dr.nextDouble();
+
+}
